92d86ccc9d73c73cd0f4742f930c0e14195f5f75
Pennone <fabio.pennone@mail.polimi.it>
2022-09-30 22:32:48 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,10 @@ function preload() {
   // put preload code here
 }
 
+let random1;
+
+let random2;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
@@ -10,54 +14,28 @@ function setup() {
 
   angleMode(DEGREES);
   frameRate(60);
-  background("pink");
+  background("white");
+  strokeWeight(10);
 
-  noStroke();
+  random1 = [random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight),];
 
-  let coloris = ["white","green", "yellow", "lightblue", "cyan", "lightgreen"];
-  let colorib = ["red", "blue", "gray", "purple", "brown", "black", "darkblue"];
+  random2 = [random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight),];
 
-  fill("orange");
-  rect(0, 0, windowWidth, 50);
-  rect(0, 75, windowWidth, 25);
-  rect(0, 110, windowWidth, 10);
-
-  fill("yellow");
-  ellipse(windowWidth/2, 75, 100);
-
-  const BaseWidth = 20;
-
-  for(let j = 0; j < 8; j++){
-    let Width = BaseWidth * (j + 1);
-    let HeightSide = Width / 3;
-    let OffsetY = Width - 25;
-    if (j > 0){
-      OffsetY *= j;
-    }
-    let Color1 = random(coloris);
-    let Color2 = random(colorib);
-    if (j < 5 && j > 0){
-      for(let i = 0; i < windowWidth; i++){
-        let Offset = Width / 2 * i;
-        fill(Color1);
-        rect(Width*i, OffsetY + Offset, Width, HeightSide);
-        fill(Color2);
-        rect(Width*i, OffsetY + HeightSide + Offset, Width, windowWidth);
-      }
-    } else {
-      for(let i = 0; i < windowWidth; i++){
-        let Offset = Width / 2 * i;
-        fill(Color1);
-        rect( windowWidth - Width*i, OffsetY + Offset, Width, HeightSide);
-        fill(Color2);
-        rect( windowWidth - Width*i, OffsetY + HeightSide + Offset, Width, windowWidth);
-      }
-    }
+  for (let i = 0; i < 8; i++){
+    console.log(random1[i], random2[i]);
   }
+
 }
 
 function draw() {
   // put drawing code here
   
-
+  for (let i = 0; i < 8; i++){
+    stroke(225-(25*i));
+    fill(225-(25*i));
+    line(0, random1[i], windowWidth, random2[i]);
+    push();
+    
+    pop();
+  }
 }
