383a83e77dd8e735067aeabfba3c4707fe491df3
Pennone <fabio.pennone@mail.polimi.it>
2022-09-30 23:08:37 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,13 +13,13 @@ function setup() {
   // rectMode(CENTER);
 
   angleMode(DEGREES);
-  frameRate(60);
+  frameRate(30);
   background("white");
-  strokeWeight(10);
+  strokeWeight(5);
 
-  random1 = [random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight),];
+  random1 = [random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight)];
 
-  random2 = [random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight),];
+  random2 = [random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight), random(0, windowHeight)];
 
   for (let i = 0; i < 8; i++){
     console.log(random1[i], random2[i]);
@@ -29,13 +29,21 @@ function setup() {
 
 function draw() {
   // put drawing code here
+
+  let cont = frameCount/600;
+
+  const sphereSize = 75;
   
-  for (let i = 0; i < 8; i++){
-    stroke(225-(25*i));
-    fill(225-(25*i));
+  for (let i = 0; i < 10; i++){
+    stroke(225-(20*i));
     line(0, random1[i], windowWidth, random2[i]);
     push();
-    
+    //ellipse(lerp(0, windowWidth, cont), lerp(random1[i], random2[i], cont), 50, 50);
+    translate(lerp(0, windowWidth, cont), lerp(random1[i], random2[i], cont));
+    fill(300);
+    ellipse(0, 0, sphereSize, sphereSize);
+    fill(225-(20*i));
+    ellipse(0, 0, sphereSize, sphereSize);
     pop();
   }
 }
