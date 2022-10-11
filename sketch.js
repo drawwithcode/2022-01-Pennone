7523a59d6337d89c363a46dbae8c4f45bdcea616
Pennone <fabio.pennone@mail.polimi.it>
2022-10-11 19:07:24 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,6 +26,15 @@ let circles = [];
 
 let numCircles = 0;
 
+let rb;
+let gb;
+let bb;
+let rbb;
+let gbb;
+let bbb;
+let timebg = 0;
+let cycle = true;
+
 function preload() {
   // put preload code here
 }
@@ -42,11 +51,22 @@ function setup() {
 
   console.log("click wherever");
 
+  rb = random(255);
+  gb = random(255);
+  bb = random(255);
+  rbb = random(255);
+  gbb = random(255);
+  bbb = random(255);
+
   circles.push(new Circles(random(windowWidth), random(windowHeight), random(20), random(2), random(2)));
 }
 
 function draw() {
   // put drawing code here
+
+  timebg += 0.006;
+
+  background(lerp(rb, rbb, frameCount/150), lerp(gb, gbb, frameCount/150), lerp(bb,bbb, frameCount/150));
 
   for(let i = 0; i < numCircles + 1; i++){
     circles[i].move();
